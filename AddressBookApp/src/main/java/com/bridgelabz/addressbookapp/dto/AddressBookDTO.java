9b20f9763f130cec6549d699bfee338badc567ee
Abhishek <you@example.com>
2022-10-20 20:56:33 +0530

    Added:[Abhishek] Auto generate getters and setters using lombok library in DTO

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.addressbookapp.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookDTO {
     private String firstName;
     private String lastName;
@@ -8,76 +16,4 @@ public class AddressBookDTO {
     private int zip;
     private String email;
     private long contact;
-
-
-    public AddressBookDTO() {
-    }
-
-    public AddressBookDTO(long id, String firstName, String lastName, String city, String state,
-                       int zip, long contact, String email) {
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.email = email;
-        this.contact = contact;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getContact() {
-        return contact;
-    }
-
-    public void setContact(long contact) {
-        this.contact = contact;
-    }
 }
