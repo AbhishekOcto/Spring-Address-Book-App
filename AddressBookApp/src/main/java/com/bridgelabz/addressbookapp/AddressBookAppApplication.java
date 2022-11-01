249c9f72642cbd9b12804955f761b3de019434a4
Abhishek <you@example.com>
2022-11-01 18:56:24 +0530

    Added:[Abhishek] Send Email Using SMTP JMS

--- AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,19 +1,31 @@
 package com.bridgelabz.addressbookapp;
 
+import com.bridgelabz.addressbookapp.service.EmailSenderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 //@EnableSwagger2
 @SpringBootApplication
 public class AddressBookAppApplication {
+    @Autowired
+    private EmailSenderService senderService;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
         log.info("Employee payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail(){
+        senderService.sendEmail("abhishek.mce13@gmail.com",
+                "This is subject",
+                "This is body of the mail");
     }
 
 }
